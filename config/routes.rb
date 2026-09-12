9ea822b939f79a4399e9086eb3614edebdfded3c
Nicole Weiss <nweiss1208@tamu.edu>
2026-09-11 21:28:46 -0500

    Complete Book Collection CRUD functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    member do
+      get :delete
+    end
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -11,5 +14,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "books#index"
 end
